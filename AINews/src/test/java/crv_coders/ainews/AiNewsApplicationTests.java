5e18054908d2f5d124f67f81b282d7665d3cd3b4
Mihai Patru <mihaipatru05@gmail.com>
2024-12-07 17:43:11 +0200

    Created the project structure

---
+++ AINews/src/test/java/crv_coders/ainews/AiNewsApplicationTests.java
@@ -0,0 +1,13 @@
+package crv_coders.ainews;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AiNewsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
